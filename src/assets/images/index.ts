7cd62db0860ce50444934d6c84c80287b3ffa9ae
Ygor Salles <ygorsallesaniceto@gmail.com>
2024-11-18 14:46:47 -0300

    fix: responsive header and footer

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,7 +1,9 @@
 import logoTmdb from "./svg/logo-tmdb.svg";
+import tech4 from "./jpeg/tech4humans_hyperautomation_logo.jpeg";
 import notFoundImage from "./jpg/not-found-image.jpg";
 
 export const IMAGES = {
   logoTmdb,
   notFoundImage,
+  tech4,
 };
--- src/components/layout/BaseLayout/index.tsx
+++ src/components/layout/BaseLayout/index.tsx
@@ -4,15 +4,23 @@ import { IBaseLayoutProps } from "./types";
 export function BaseLayout({ children }: Readonly<IBaseLayoutProps>) {
   return (
     <div className="h-screen flex flex-col overflow-x-hidden">
-      <header className="flex justify-between items-center  bg-zinc-900 p-4">
-        <img src={IMAGES.logoTmdb} alt="logo" className="w-24 h-12" />
-        <span className="text-white">Titulo</span>
+      <header className="flex justify-between items-center  bg-zinc-900 py-4 px-4 sm:px-8">
+        <img src={IMAGES.logoTmdb} alt="tmdb-logo" className="w-24 h-12" />
+        <div className="flex items-center gap-2">
+          <img
+            src={IMAGES.tech4}
+            alt="tech4Humans-logo"
+            className="w-8 h-8 sm:w-10h-10"
+          />
+          <span className="text-white">Buscador de filmes</span>
+        </div>
       </header>
       <main className="flex flex-col gap-6 flex-1 items-center bg-gradient-to-r py-5 from-zinc-800 to-zinc-900 via-blue-900">
         {children}
       </main>
-      <footer className="flex justify-center items-center  bg-blue-950 p-12">
-        <span className="text-white">Footer</span>
+      <footer className="flex flex-col gap-2 justify-center items-center  bg-blue-950 p-4 sm:p-8">
+        <span className="text-white">Desenvolvido por Ygor Salles</span>
+        <span className="text-white">Tech 4 Humans</span>
       </footer>
     </div>
   );
